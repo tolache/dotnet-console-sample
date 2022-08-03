22ae4b113faa48fcce8cfdfed8ecad66c2ae600d
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-08-03 10:09:41 +0200

    recreate solution, and project with -f net5.0

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 
-class Program {
-	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
-	}
+namespace dotnet_console_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
 }
