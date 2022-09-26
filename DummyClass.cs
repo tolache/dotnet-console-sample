6f904be393459f9c0f662cd87fed173464342113
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-09-26 16:55:48 +0200

    add a class with a nullable string prop

---
+++ DummyClass.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_console_sample;
+
+public sealed class DummyClass {
+    public decimal Amount { get; set; }
+    public Guid Id { get; set; }
+    public int Value { get; set; }
+    public string? Name { get; set; }
+}
