31244af099b59bcd472ecbc3f34a2002406cfc62
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-10-04 16:52:32 +0200

    add a class with nullable string

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace dotnet_console_sample
         {
             Console.WriteLine("Hello World!");
             Source source = new Source();
+            Console.WriteLine(source);
         }
+    }
+
+    public sealed class Source {
+        public decimal Amount { get; set; }
+        public Guid Id { get; set; }
+        public int Value { get; set; }
+        public string? Name { get; set; }
     }
 }
